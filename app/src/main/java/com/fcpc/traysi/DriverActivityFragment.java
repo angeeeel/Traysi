00ffc8c032fc03da000c6f6da5e322f97f2911e8
LAB3 <angeeeeeel.g@gmaikl>
2019-10-16 18:44:48 +0800

    update 2

---
+++ app/src/main/java/com/fcpc/traysi/DriverActivityFragment.java
@@ -0,0 +1,23 @@
+package com.fcpc.traysi;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DriverActivityFragment extends Fragment {
+
+    public DriverActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_driver, container, false);
+    }
+}
--- app/src/main/java/com/fcpc/traysi/UserSelectionActivity.java
+++ app/src/main/java/com/fcpc/traysi/UserSelectionActivity.java
@@ -2,13 +2,52 @@ package com.fcpc.traysi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class UserSelectionActivity extends AppCompatActivity {
+
+    private ListView lvUserTypes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_selection);
+
+        initListView();
+    }
+
+    private void initListView(){
+
+        lvUserTypes = findViewById(R.id.lvListType);
+
+        String[] items = {"Driver","Passenger"};
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        lvUserTypes.setAdapter(itemsAdapter);
+
+        lvUserTypes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        startActivity(new Intent().setClass(getApplicationContext(),DriverActivity.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent().setClass(getApplicationContext(),PassengerActivity.class));
+                        break;
+                }
+            }
+        });
+
+
+
     }
 }
